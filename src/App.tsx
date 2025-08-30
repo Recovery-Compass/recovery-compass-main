62e8674133bb0cdbab1cb2ed5caa62ad6d904547
Eric Jones <eric@recovery-compass.org>
2025-08-30 18:57:37 -0500

    Minimal app refactor: archive non-essential pages and prune routes to core flows

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,11 @@ import InvestorNavigation from "./components/InvestorNavigation";
 import Footer from "./components/Footer";
 import { AuthProvider } from "./hooks/useAuth";
 import VideoHero from "./components/VideoHero";
-import Auth from "./pages/Auth";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfService from "./pages/TermsOfService";
-import CompassCompanion from "./pages/CompassCompanion";
-import ImpactTranslator from "./pages/ImpactTranslator";
-import PartnershipProposals from "./pages/PartnershipProposals";
 import PathwaySelect from "./pages/PathwaySelect";
-import EnvironmentalDesign from "./pages/EnvironmentalDesign";
 import EnvironmentalDesignAdaptive from "./pages/EnvironmentalDesignAdaptive";
-import AssessmentTransition from "./components/AssessmentTransition";
-import MakeIntegration from "./pages/MakeIntegration";
-import WhittierDashboard from "./pages/WhittierDashboard";
 import NotFound from "./pages/NotFound";
-import WFDAttachmentSuite from "./components/WFDAttachmentSuite";
-import ComplianceCompass from "./pages/ComplianceCompass";
-import FirstExhibit from "./pages/FirstExhibit";
-import Methodology from "./pages/Methodology";
 import Adventure from "./pages/Adventure";
 import Begin from "./pages/Begin";
-// Investor Pages
-import InvestorPitch from "./pages/InvestorPitch";
-import MarketAnalysis from "./pages/MarketAnalysis";
-import SolutionDemo from "./pages/SolutionDemo";
-import BusinessModel from "./pages/BusinessModel";
-import ImpactMetrics from "./pages/ImpactMetrics";
-import Roadmap from "./pages/Roadmap";
-import { TestBreathSync } from "./pages/TestBreathSync";
 import { EnvironmentalQuizStandalone } from "./pages/EnvironmentalQuizStandalone";
 import { trackPageView, trackBusinessEvent } from "./lib/analytics";
 
@@ -117,73 +95,10 @@ function App() {
           <BrowserRouter>
             <AnalyticsWrapper />
             <Routes>
-              {/* Authentication Route */}
-              <Route path="/auth" element={<Auth />} />
-            {/* Investor Relations Routes */}
-            <Route path="/investor-pitch" element={
-              <div className="min-h-screen bg-navy">
-                <InvestorNavigation />
-                <InvestorPitch />
-              </div>
-            } />
-            <Route path="/market-analysis" element={
-              <div className="min-h-screen bg-navy">
-                <InvestorNavigation />
-                <MarketAnalysis />
-              </div>
-            } />
-            <Route path="/solution-demo" element={
-              <div className="min-h-screen bg-navy">
-                <InvestorNavigation />
-                <SolutionDemo />
-              </div>
-            } />
-            <Route path="/business-model" element={
-              <div className="min-h-screen bg-navy">
-                <InvestorNavigation />
-                <BusinessModel />
-              </div>
-            } />
-            <Route path="/impact-metrics" element={
-              <div className="min-h-screen bg-navy">
-                <InvestorNavigation />
-                <ImpactMetrics />
-              </div>
-            } />
-            <Route path="/roadmap" element={
-              <div className="min-h-screen bg-navy">
-                <InvestorNavigation />
-                <Roadmap />
-              </div>
-            } />
-
-            {/* Main Recovery Compass Routes */}
+            {/* Minimal app routes */}
             <Route path="/" element={<VideoHero />} />
             <Route path="/begin" element={<Begin />} />
-            <Route path="/compass-companion" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <CompassCompanion />
-                </div>
-              </>
-            } />
-            <Route path="/impact-translator" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <ImpactTranslator />
-                </div>
-              </>
-            } />
-            <Route path="/partnership-proposals" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <PartnershipProposals />
-                </div>
-              </>
-            } />
+            <Route path="/adventure" element={<Adventure />} />
             <Route path="/pathway-select" element={
               <>
                 <Navigation />
@@ -192,6 +107,7 @@ function App() {
                 </div>
               </>
             } />
+            <Route path="/environmental-quiz" element={<EnvironmentalQuizStandalone />} />
             <Route path="/environmental-design" element={
               <>
                 <Navigation />
@@ -200,68 +116,6 @@ function App() {
                 </div>
               </>
             } />
-            <Route path="/environmental-design-static" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <EnvironmentalDesign />
-                </div>
-              </>
-            } />
-            <Route path="/assessment-transition" element={<AssessmentTransition />} />
-            <Route path="/environmental-quiz" element={<EnvironmentalQuizStandalone />} />
-            <Route path="/make-integration" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <MakeIntegration />
-                </div>
-              </>
-            } />
-            <Route path="/whittier-dashboard" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <WhittierDashboard />
-                </div>
-              </>
-            } />
-            <Route path="/compliance-compass" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <ComplianceCompass />
-                </div>
-              </>
-            } />
-            <Route path="/first-exhibit" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <FirstExhibit />
-                </div>
-              </>
-            } />
-            <Route path="/methodology" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <Methodology />
-                </div>
-              </>
-            } />
-            <Route path="/adventure" element={
-              <>
-                <Navigation />
-                <div className="pt-20">
-                  <Adventure />
-                </div>
-              </>
-            } />
-            <Route path="/wfd-attachments" element={<WFDAttachmentSuite />} />
-            <Route path="/test-breathsync" element={<TestBreathSync />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
